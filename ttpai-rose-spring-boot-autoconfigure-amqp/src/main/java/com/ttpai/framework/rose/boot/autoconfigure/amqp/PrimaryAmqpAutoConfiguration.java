478d0ab0a47a2e2aca2374a3d173f288d12d8664
kaibin.yang <kaibin.yang@ttpai.cn>
2021-02-09 13:02:08 +0800

    配置提示

--- ttpai-rose-spring-boot-autoconfigure-amqp/src/main/java/com/ttpai/framework/rose/boot/autoconfigure/amqp/PrimaryAmqpAutoConfiguration.java
+++ ttpai-rose-spring-boot-autoconfigure-amqp/src/main/java/com/ttpai/framework/rose/boot/autoconfigure/amqp/PrimaryAmqpAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.ttpai.framework.rose.boot.autoconfigure.amqp;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 public class PrimaryAmqpAutoConfiguration {
 
     @Bean
+    @ConditionalOnProperty(name = "rose.boot.autoconfigure.amqp.primary", havingValue = "true", matchIfMissing = true)
     @ConditionalOnClass(name = "org.springframework.amqp.rabbit.connection.ConnectionFactory")
     public PrimaryAmqpFactoryPostProcessor primaryAmqpFactoryPostProcessor() {
         return new PrimaryAmqpFactoryPostProcessor();
